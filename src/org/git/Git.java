093e0517735c04b5c297068192095753078d2ce6
Gauthamkumar91 <kgowtham12mech@gmail.com>
2022-08-06 13:04:03 +0530

    testtwo

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -1,5 +1,12 @@
 package org.git;
 
 public class Git {
-
+	public void saving() {
+		System.out.println("saving  deposite  10000");
+	}	
+	public void fixed() {
+		System.out.println("fixed deposite 5000");
+		System.out.println("fixed deposite 5000");
+		System.out.println("fixed deposite 5000");
+	}
 }
